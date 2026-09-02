8ad7c8a9fc62446beb6f91fc424fe4eddc666c0c
jpjosey <92425017+jpjosey@users.noreply.github.com>
2026-09-02 17:37:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from supabase import create_client
 sb = create_client(st.secrets["SUPABASE_URL"], st.secrets["SUPABASE_KEY"])
 
 TILE_DATA = Path("tiles/tiles.json")
-IMAGE_DIR = Path("tiles/images")
+IMAGE_DIR = Path("tiles")
 ICON_DIR = Path("icons")
 
 
